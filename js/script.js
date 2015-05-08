38ef2ea99d316ebabf9b283062bb149b34773437
Ray Tsai <tjrtsai@gmail.com>
2015-05-08 01:27:07 -0400

    Added parallax and fixed vendor icons

--- js/script.js
+++ js/script.js
@@ -2,12 +2,26 @@
 var photos = document.querySelector('.photos');
 var photosGrid = document.querySelector('.photos-grid');
 
+window.photos = photos;
+window.photosGrid = photosGrid;
+
 document.addEventListener('scroll', function() {
     var windowHeight = window.innerHeight;
     var photosHeight = photos.clientHeight
-    var referencePt = (windowHeight - photosHeight) / 2 / windowHeight;
+    var windowMidPt = (windowHeight - photosHeight) / 2 / windowHeight;
 
-    console.log(referencePt);
+    var photosRect = photos.getBoundingClientRect();
+    var photosTop = photosRect.top;
+    var photosMidPt = photosRect.top + photos.clientHeight / 2;
 
-    // var photosTop = photos.getBoundingClientRect().top;
+    var x1 = -photos.clientHeight / 2;
+    var y1 = photos.clientHeight;
+    var x2 = window.innerHeight + photos.clientHeight / 2;
+    var y2 = -photosGrid.clientHeight;
+    var slope = (y2 - y1) / (x2 - x1);
+    var intercept = photos.clientHeight * (1 + slope / 2);
+
+    var offset = photosMidPt * slope + intercept;
+
+    photosGrid.style['transform'] = 'translateY(' + offset + 'px)';
 })
